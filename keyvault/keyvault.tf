253209e7fc659fde9f78e590320785c0554efceb
MauroAlan <mauro-alan3@hotmail.com>
2025-03-17 17:01:04 -0300

    key-vault ok, key vault exposto

--- keyvault/keyvault.tf
+++ keyvault/keyvault.tf
@@ -1,8 +1,7 @@
 resource "azurerm_key_vault" "kv" {
   name                        = "kv-desafio-aks"
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
-  enabled_for_disk_encryption = true
+  location                    = azurerm_resource_group.rg.location  
+  resource_group_name         = azurerm_resource_group.rg.name      
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
 
@@ -14,4 +13,9 @@ resource "azurerm_key_vault" "kv" {
       "Get", "List", "Set", "Delete"
     ]
   }
+}
+resource "azurerm_key_vault_secret" "db_connection_string" {
+  name         = "db-connection-string" 
+  value        = azurerm_mssql_database.db.connection_string
+  key_vault_id = azurerm_key_vault.kv.id 
 }
