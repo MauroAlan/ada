e3a8c3921eddb2b21837975c10cf869b41ff4074
MauroAlan <mauro-alan3@hotmail.com>
2025-03-17 15:23:23 -0300

    sem terraform

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "grupo-recurso-ada" {
-  name     = "grupo-de-recurso-ada"
+resource "azurerm_resource_group" "grupo-recursos-ada" {
+  name     = "grupo-de-recursos-ada"
   location = "brazilsouth"
 }
---
+++ providers.tf
@@ -0,0 +1,5 @@
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b9879c18-e8da-4c85-a9de-06462b9cb8aa"
+}
