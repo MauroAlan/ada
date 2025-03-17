9b0963d8bde1c0a3392e01f98445347f52b271b0
MauroAlan <mauro-alan3@hotmail.com>
2025-03-17 15:42:26 -0300

    aks criado

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
-resource "azurerm_resource_group" "grupo-recursos-ada" {
-  name     = "grupo-de-recursos-ada"
-  location = "brazilsouth"
+data "azurerm_resource_group" "grupo-recursos-ada" {
+  name = "grupo-de-recursos-ada"
+}
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "aks-cluster"
+  location            = data.azurerm_resource_group.grupo-recursos-ada.location
+  resource_group_name = data.azurerm_resource_group.grupo-recursos-ada.name
+  dns_prefix          = "aks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    environment = "production"
+  }
 }
